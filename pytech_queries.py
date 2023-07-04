cfc017a030e901290252b8cbec1fbcbf270f23cf
Tzvi20 <124609991+Tzvi20@users.noreply.github.com>
2023-07-04 07:49:22 -0400

    Add files via upload

---
+++ pytech_queries.py
@@ -0,0 +1,22 @@
+from pymongo import MongoClient
+mongodb_url = 'mongodb+srv://admin:admin@cluster0.cjljgrs.mongodb.net/'
+
+client = MongoClient(mongodb_url)
+
+
+db = client["pytech"]
+
+students = db["students"]
+fred = {
+    "first_name": "Fred"
+}
+fred_student_id = students.insert_one(fred).inserted_id
+print(fred_student_id)
+
+new_students = [
+    {"student_id": "1007", "first_name": "Alice"},
+    {"student_id": "1008", "first_name": "Bob"},
+    {"student_id": "1009", "first_name": "Charlie"}
+]
+doc = students.find_one({"student_id": "1007"})
+print(doc["student_id"])
